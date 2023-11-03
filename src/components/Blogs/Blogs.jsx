bc2210364828fb77ec60c160a1e5aff900ead6c1
Saeedullah15 <saeedullahazim65@gmail.com>
2023-11-03 11:36:16 +0600

    first commit

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const Blogs = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        fetch('blogs.json')
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [])
+
+    return (
+        <div>
+            <h3 className="text-2xl">blogs: {blogs.length}</h3>
+        </div>
+    );
+};
+
+export default Blogs;
